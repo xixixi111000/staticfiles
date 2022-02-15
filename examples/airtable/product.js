e5c7366d2779ee732c6e75a5173eef2d7992d1d8
xixixi111000 <95582363+xixixi111000@users.noreply.github.com>
2022-02-15 12:32:07 -0500

    Update product.js

--- examples/airtable/product.js
+++ examples/airtable/product.js
@@ -8,18 +8,20 @@ const fetchProduct = async () => {
 
     const {
       data: { fields },
+    // } = await axios.get(`https://frosty-agnesi-b76b1a.netlify.app/api/catdog${id}`)
     } = await axios.get(`/api/catdog${id}`)
-    const { name, price, image, description } = fields
+    const { name, price, images, description, colors, reviews } = fields
     result.innerHTML = `<h1 class="title">${name}</h1>
   <article class="product">
     <img class="product-img"
-    src="${image[0].url}"
+    src="${images[0].url}"
     alt="${name}"
     />
     <div class="product-info">
       <h5 class="title">${name}</h5>
       <h5 class="price">$${price}</h5>
       <p class="desc">${description}</p>
+      <p>${colors}</p>
     </div>
   </article>`
   } catch (error) {
